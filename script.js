823210a8aad6a4d3e157f670c5436bb5da2055c4
Busola Odoko <rachealodoko@gmail.com>
2020-12-15 19:26:37 +0100

    made changes

--- script.js
+++ script.js
@@ -1,7 +1,61 @@
 
+let allAboutTexts = [...document.querySelectorAll(".abt-t")];
+let allAboutBtns = [...document.querySelectorAll(".abt-btns")];
+let parentbtns =document.querySelector('.texts-btns');
+
+const para = document.querySelector(".info");
+para.innerHTML = allAboutTexts[0].innerHTML;
 
 
-function clicked(num){
-    num >=0 ? classNM"bg-active": "bg-inactive"
+
+
+function AboutMeClicks(num) {
+  let demo = document.querySelector(".info");
+  if ((num >= 0 && allAboutTexts.length)) {
+    demo.innerHTML = allAboutTexts[num].innerHTML;
+     parentbtns.querySelector('.active').classList.remove('active');
+     allAboutBtns[num].classList.add('active');
+  } else {
+    return  
+  }
 
 }
+// let defaultImage = document.querySelector('.default-image');
+
+
+// let images = ['imageofme2.png','imageofme3.png','imageofme4.png']
+// let i =0
+
+
+// function prev(){
+// if (i<=0) i=images.length;
+// i--;
+// return setImg();
+// }
+
+
+// function next(){
+// if(i>=images.length -1) i=-1;
+// i++;
+// return setImg();
+// }
+
+// function setImg(){
+
+//   return defaultImage.setAttribute('src',`./images/${images[i]}`)
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
